97581abf1a0e37f2c7ebccd46ee435eabb711e16
Kamlendra Rathour <421kamlendra@gmail.com>
2021-05-12 17:39:39 +0530

    update

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -4,6 +4,7 @@ import io from 'socket.io-client';
 
 import './Chat.css';
 import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
 
 let socket;
 
@@ -51,7 +52,7 @@ const Chat = ({location}) => {
         <div className="outerContainer">
             <div className="container">
                 <InfoBar room ={room} />
-                <Input />
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
             </div>
         </div>
     )
